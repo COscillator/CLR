827ac4329398adf819adc2147ee675ede9482427
COscillator <45717556+COscillator@users.noreply.github.com>
2021-08-13 22:16:46 +0800

    Add files via upload

---
+++ MEFSample1/ExtendedOperations/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+
+namespace ExtendedOperations
+{
+    [Export(typeof(SimpleCalculator.IOperation))]
+    [ExportMetadata("Symbol", '%')]
+    public class Mod : SimpleCalculator.IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left % right;
+        }
+    }
+}
---
+++ MEFSample1/SimpleCalculator/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace SimpleCalculator
+{
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '-')]
+    class Subtract : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left - right;
+        }
+    }
+
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '+')]
+    class Add : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left + right;
+        }
+    }
+    public interface IOperation
+    {
+        int Operate(int left, int right);
+    }
+
+    public interface IOperationData
+    {
+        char Symbol { get; }
+    }
+    public interface ICalculator
+    {
+        string Calculate(string input);
+    }
+
+    [Export(typeof(ICalculator))]
+    class MySimpleCalculator : ICalculator
+    {
+        [ImportMany]
+        IEnumerable<Lazy<IOperation, IOperationData>> operations;
+        public String Calculate(string input)
+        {
+            int left;
+            int right;
+            char operation;
+            // Finds the operator.
+            int fn = FindFirstNonDigit(input);
+            if (fn < 0) return "Could not parse command.";
+
+            try
+            {
+                // Separate out the operands.
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch
+            {
+                return "Could not parse command.";
+            }
+
+            operation = input[fn];
+
+            foreach (Lazy<IOperation, IOperationData> i in operations)
+            {
+                if (i.Metadata.Symbol.Equals(operation))
+                {
+                    return i.Value.Operate(left, right).ToString();
+                }
+            }
+            return "Operation Not Found!";
+        }
+        private int FindFirstNonDigit(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!char.IsDigit(s[i])) return i;
+            }
+            return -1;
+        }
+    }
+    class Program
+    {
+        private CompositionContainer _container;
+        private Program()
+        {
+            try
+            {
+                // An aggregate catalog that combines multiple catalogs.
+                var catalog = new AggregateCatalog();
+                // Adds all the parts found in the same assembly as the Program class.
+                catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+                catalog.Catalogs.Add(new DirectoryCatalog("C:\\Users\\COscillator\\source\\repos\\SimpleCalculator\\SimpleCalculator\\Extensions"));
+                // Create the CompositionContainer with the parts in the catalog.
+                _container = new CompositionContainer(catalog);
+                _container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+
+        }
+
+        [Import(typeof(ICalculator))]
+        public ICalculator calculator;
+
+
+        static void Main(string[] args)
+        {
+            // Composition is performed in the constructor.
+            var p = new Program();
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                string s = Console.ReadLine();
+                Console.WriteLine(p.calculator.Calculate(s));
+            }
+        }
+    }
+}
